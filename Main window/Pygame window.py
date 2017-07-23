aa589a7e40930471ebeca15ba4e9aa4d75a77ce3
Anthony Perez <stockcart21@yahoo.com>
2017-07-23 10:57:06 -0700

    Update Pygame window.py

--- Main window/Pygame window.py
+++ Main window/Pygame window.py
@@ -5,11 +5,9 @@ def run_game():
     pygame.init()
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
-    
     #Start the main loop for the game.
     while True:
-    
-        # Watch for keyboard and mouse Events.
+    # Watch for keyboard and mouse Events.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
               sys.exit()
