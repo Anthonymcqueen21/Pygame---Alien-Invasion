15094cff5d842c6c529096fc7b94f5d1fe7e574f
Anthony Perez <stockcart21@yahoo.com>
2017-07-30 14:54:58 -0700

    Update Scoreboard.py

--- Scoring/Scoreboard.py
+++ Scoring/Scoreboard.py
@@ -14,8 +14,7 @@ class Scoreboard():
       # Prepare the initial score image/
       self.prep_score()
       self.prep_high_score()
-      
-  def prep_high_score(self):
+ def prep_high_score(self):
      """Turn the high score into a rendered image."""
      high_score = int(round(self.stats.high_score, -1))
      high_score_str = "(:,)".format(high_score)
