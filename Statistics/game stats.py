2632eae61c1c4fba6c4aaa5e5e0a4e16a753f889
Anthony Perez <stockcart21@yahoo.com>
2017-08-01 15:04:31 -0700

    Create game stats.py
    Tracks all alien invasion.

---
+++ Statistics/game stats.py
@@ -0,0 +1,16 @@
+class GameStats():
+  """Track statistics for Alien Invasion."""
+  
+  def __init__(self, ai_settings):
+    """Inititialize statistics."""
+    self.ai_settings = ai_settings
+    self.reset_stats()
+    
+    # Start game in an inactive state.
+    self.game_active = False
+    
+  def reset_stats(self):
+    """Initialize statistics that can change during the game."""
+    self.ships_left = self.ai_settings.ship_limit
+    self.score = 0
+    self.level = 1
