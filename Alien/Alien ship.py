dcb56c56dc01ef4d0091d8830e8c6af2407a1bee
Anthony Perez <stockcart21@yahoo.com>
2017-08-02 16:28:49 -0700

    Update Alien ship.py

--- Alien/Alien ship.py
+++ Alien/Alien ship.py
@@ -11,7 +11,6 @@ class Alien(Sprite):
     #Loading the alien image and set its rect attribute.
     self.image = pygame.image.load('images/alien.bmp')
     self.rect = self.image.get_rect()
-    
     #Start new alien near the top left of the screen.
     self.rect.x = self.rect.width
     self.rect.y = self.rect.height
