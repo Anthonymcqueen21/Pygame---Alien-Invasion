453e0df00697e202feef6c3437b15aefebd3fc98
Anthony Perez <stockcart21@yahoo.com>
2017-08-06 08:34:21 -0700

    Update Scoreboard.py

--- Scoring/Scoreboard.py
+++ Scoring/Scoreboard.py
@@ -1,5 +1,6 @@
 import pygame.font
 class Scoreboard():
+  
   """A class to report scoring information."""
   def __init__(self, ai_settings, screen, stats):
       """Initiatize scorekeeping attributes."""
