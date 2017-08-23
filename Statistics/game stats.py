8fb3dfe220156bf71878bc7b64e75e9cc451265d
Anthony Perez <stockcart21@yahoo.com>
2017-08-23 10:57:17 -0700

    Update game stats.py

--- Statistics/game stats.py
+++ Statistics/game stats.py
@@ -1,14 +1,14 @@
 class GameStats():
   """Track statistics for Alien Invasion."""
   
-  def __init__(self, ai_settings):
+ def __init__(self, ai_settings):
     """Inititialize statistics."""
     self.ai_settings = ai_settings
     self.reset_stats()
      # Start game in an inactive state.
     self.game_active = False
     
-  def reset_stats(self):
+ def reset_stats(self):
     """Initialize statistics that can change during the game."""
     self.ships_left = self.ai_settings.ship_limit
     self.score = 0
