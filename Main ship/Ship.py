7bd27292ebe627cebfa3d94d597358d82a9aa1e1
Anthony Perez <stockcart21@yahoo.com>
2017-08-30 22:51:54 -0700

    Update Ship.py

--- Main ship/Ship.py
+++ Main ship/Ship.py
@@ -51,7 +51,6 @@ class Ship():
   def blitme(self):
     """Draw the ship at its current form and location."""
     self.screen.blit(self.image, self.rect)
-    
   def center_ship(self):
     """Center the ship on the screen."""
     self.center = self.screen_rect.centerx
