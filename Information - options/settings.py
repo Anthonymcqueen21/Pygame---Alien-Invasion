d10762438b8680f886b406baa96eb10c58f6cf5b
Anthony Perez <stockcart21@yahoo.com>
2017-09-20 13:47:22 -0700

    Update settings.py

--- Information - options/settings.py
+++ Information - options/settings.py
@@ -20,6 +20,7 @@ class Settings():
    self.initialize_dynamic_settings(self):
       """Increase speed settings and alien point values."""
       """Initialize settings that change throughout the game."""
+      
       self.ship_speed_factor *= self.speedup_scale
       self.bullet_speed_factor *= self.speedup_scale
       self.alien_speed_factor *= self.speedup_scale
