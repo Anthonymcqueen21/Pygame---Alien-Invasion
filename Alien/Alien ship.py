aa9a2df8a5b452f44678f9c156dc4beb845a1f49
Anthony Perez <stockcart21@yahoo.com>
2017-09-30 12:48:01 -0700

    Update Alien ship.py

--- Alien/Alien ship.py
+++ Alien/Alien ship.py
@@ -2,7 +2,7 @@ import pygame
 from pygame.sprite import Sprite
 class Alien(Sprite):
   
-  """A class to represent a single alien in the fleet."""
+"""A class to represent a single alien in the fleet."""
   
   def __init__(self, ai_settings, screen):
     """Initialize the alien and set its starting a position."""
