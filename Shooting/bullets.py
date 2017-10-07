ecdb64921e2d2eb8254dc69b5699c66685390d23
Anthony Perez <stockcart21@yahoo.com>
2017-10-07 15:29:48 -0700

    Update bullets.py

--- Shooting/bullets.py
+++ Shooting/bullets.py
@@ -3,6 +3,7 @@ from pygame.sprite import Sprite
 
 class Bullet(Sprite):
   """A class to manage all the bullets that will be fired from the ship."""
+  """Class centers reaction x and initiate bullets."""
   
   def __init__(self, ai_settings, screen, ship):
     """Create a bullet object at the ships current position."""
