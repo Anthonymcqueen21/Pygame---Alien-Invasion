5ad0e7c31323bc382a492a25c1e6d4a764818001
Anthony Perez <stockcart21@yahoo.com>
2018-01-08 19:57:07 -0800

    Update bullets.py

--- Shooting/bullets.py
+++ Shooting/bullets.py
@@ -22,13 +22,13 @@ class Bullet(Sprite):
     self.color = ai_settings.bullet_color
     self.speed_factor = ai_settings.bullet_speed_factor.
     
-def update(self):
+ def update(self):
   """Move the bullet up the screen."""
   #Update the decimal position of the bullet.
   self.y -= self.speed_factor
   #Update the rect position.
   self.rect.y = self.y
   
-def draw_bullet(self):
+ def draw_bullet(self):
   """Draw the bullet to the screen."""
   pygame.draw.rect(self.screen, self.color, self.rect)
